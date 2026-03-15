82105f076152c3125302b213546fe3b40b9092d3
ridhukhan <prakritirshandhane@gmail.com>
2026-03-15 10:04:37 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { UserProvider } from "./context/AuthContext.jsx";
 import { SocketProvider } from "./context/SocketContext.jsx";
-
+import "./index.css"
 createRoot(document.getElementById("root")).render(
   <StrictMode>
    <UserProvider>
